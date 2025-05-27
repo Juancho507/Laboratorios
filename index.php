4a3af3e3d3a3092803fa82a5550720d915757b3e
Juancho507 <judaorgor0417@gmail.com>
2025-05-26 21:17:37 -0500

    se agrega

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require ("logica/EstadoCita.php");
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Matasanos EPS</title>
 
-<!-- Bootstrap -->
+<!-- Bootstrap jajajaj -->
 <link
 	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
 	rel="stylesheet">
